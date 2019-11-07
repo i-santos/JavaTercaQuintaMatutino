548f5c97ef8788f8552529902d5ec864f551972d
i. dos santos <iosorio.santos@gmail.com>
2019-11-07 11:57:37 -0300

    Aula 10 - 07/11/2019

--- Aula 9 - 2019-11-05/Pratica3/Atividade4/src/atividade4/Usuario.java
+++ Aula 9 - 2019-11-05/Pratica3/Atividade4/src/atividade4/Usuario.java
@@ -8,18 +8,19 @@ public class Usuario {
 
     
 
-    String pedirNome() {
+        String pedirNome() {
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Digite o nome: ");
         String nome = sc.nextLine();
          return nome;
-    String pedirEmail(){
+        String pedirEmail
         System.out.println("Digite o email: ");
         String email = sc.nextLine();
         return email;
      } 
     }
 
+    
    
-}
+
